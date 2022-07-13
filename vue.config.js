6e82598caf766c8cd57c9f2cb13769935e4100eb
nasu <nararara701@gmail.com>
2022-07-13 18:11:55 +0900

    first commit

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  publicPath: '/~s22669kh/dist/',
+  transpileDependencies: [
+    'vuetify'
+  ]
 })
