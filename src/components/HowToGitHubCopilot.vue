fdff8840f13647a547d697afde5f8b8c2bd97bcd
nasu <nararara701@gmail.com>
2022-07-13 22:32:27 +0900

    OK

--- src/components/HowToGitHubCopilot.vue
+++ src/components/HowToGitHubCopilot.vue
@@ -11,23 +11,27 @@
             <v-img src="../assets/image_biggest.png"></v-img>
             <p class="text-h2 font-weight-bold text-center">の開発者コミュニティ</p>
 
-            <v-list class="text-h1">
-              <v-list-item>4000万人</v-list-item>
-              <v-list-item>210万の組織が利用</v-list-item>
-            </v-list>
+            <v-row justify="center" class="text-h1 text-center">
+              <v-col>
+                <p class="text-center">4000万人  210万の組織</p>
+              </v-col>
+            </v-row>
             <!--          <p class="text-h4">世界中のたくさんのプログラムがある！</p>-->
-            <p class="text-h4">たとえば…</p>
-            <v-container>
-              <v-row v-for="example in examplesGitHub" :key="example.name">
-                <v-col cols="6" class="text-h2 font-weight-bold pa-0">
-                  <a :href="example.pageSource">{{ example.name }}</a>
-                  <!--                <v-img :src=example.imgSource-->
-                  <!--                       :alt='`GitHubに投稿されている${example.name}のプログラムです。`'-->
-                  <!--                       width=auto></v-img>-->
-                  <!--                出典：<a>{{ example.pageSource }}</a>-->
-                </v-col>
-              </v-row>
-            </v-container>
+            <v-row>
+              <v-col cols="7">
+                <p class="text-h4">たとえば…</p>
+                <v-container>
+                  <v-row v-for="example in examplesGitHub" :key="example.name" class="pb-11">
+                    <v-col cols="6" class="text-h2 font-weight-bold pa-0">
+                      <a :href="example.pageSource">{{ example.name }}</a>
+                    </v-col>
+                  </v-row>
+                </v-container>
+              </v-col>
+              <v-col>
+                <v-img src="../assets/muryou.png" width="450"></v-img>
+              </v-col>
+            </v-row>
           </v-card-text>
         </div>
       </v-col>
@@ -38,27 +42,41 @@
           GitHub Copilotとは
         </v-card-title>
         <v-divider></v-divider>
-        <div id=""></div>
-        <v-card-text class="headline">
-          <v-img src="../assets/image_biggest.png"></v-img>
-          <p class="text-h2 font-weight-bold text-center">の開発者コミュニティ</p>
+        <div id="" class="reveal">
+          <v-card-text class="headline">
+            <v-img src="../assets/image_AI.png" width="800" class="mx-auto"></v-img>
+            <p class="text-h2 font-weight-bold text-center">によるプログラム自動補完サービス</p>
 
-          <v-list class="text-h1">
-            <v-list-item>4000万人</v-list-item>
-            <v-list-item>210万の組織が利用</v-list-item>
-          </v-list>
-          <p class="text-h4">世界中のたくさんのプログラムがある！</p>
-          たとえば…
-            <v-row v-for="example in examplesGitHub" :key="example.name">
-              <v-col cols="6">
-                <p>{{ example.name }}</p>
-                <v-img :src=example.imgSource
-                       :alt='`GitHubに投稿されている${example.name}のプログラムです。`'
-                       width=auto></v-img>
-                出典：<a>{{ example.pageSource }}</a>
+            <v-row justify="center">
+              <v-col>
+                <p class="text-center text-h2">学生なら…</p>
+              </v-col>
+              <v-col>
+                <v-img src="../assets/muryou.png" width="450"></v-img>
               </v-col>
             </v-row>
-        </v-card-text>
+
+          </v-card-text>
+        </div>
+      </v-col>
+
+
+      <v-col cols="8">
+        <v-card-title class="text-h1 font-weight-bold">
+          GitHub Copilotのしくみ
+        </v-card-title>
+        <v-divider></v-divider>
+<!--        <div id="" class="reveal">-->
+        <div>
+          <v-card-text class="headline">
+            <p class="text-h2 font-weight-bold ">GitHubにある</p>
+            <p class="text-h2 font-weight-bold text-center">膨大なプログラムを学習して予測</p>
+
+            <v-img src="../assets/GPT-3_1.png"></v-img>
+            <a href="https://www.nri.com/jp/knowledge/glossary/lst/alphabet/gpt_3">
+              野村総合研究所 "GPT-3とは"</a>から引用
+          </v-card-text>
+        </div>
       </v-col>
     </v-row>
   </v-container>
@@ -98,7 +116,10 @@ export default {
       }
     },
     EListener: function (){
-      const element
+      // const elems = document.querySelectorAll(".reveal")
+      // for (const elem in elems){
+      //   elem.addEventListener("click", this.reveal)
+      // }
       window.addEventListener("click", this.reveal)
     }
   },
